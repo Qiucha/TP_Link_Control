c40a799d30ed50062a6e894f1e7aed7bc9d8f0bb
Qiucha <qiucha86239@gmail.com>
2024-03-27 14:39:36 +0800

    first complete version, waiting for TP_Link Plug to arrive

---
+++ power-check.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+: << "About_THRESHOLD"
+This script is designed to set a charge automation, 
+LOWER_THRES is the battery level when the Plug should switch on,
+UPPER_THRES is the battery level when the Plug should switch off.
+About_THRESHOLD
+
+LOWER_THRES=20
+UPPER_THRES=80
+
+if [ LOWER_THRES -ge UPPER_THRES ]; then
+    echo "check THRESHOLD settings, probably something goes wrong."
+    exit 1
+fi
+
+
+# getting root privileges to execute tlp-stat command
+if [ $(id -u) -ne 0 ]; then
+    echo "This script requires root privileges. Please run it with sudo."
+    exit 1
+fi
+
+
+: << 'About_User_File'
+"user_file" should be created by user, which contains the information below:
+PYTHON_PATH="/path/to/user/python"
+KASA_PATH="/path/to/kasa"
+KASA_DEVICE_IP="/kasa/ip/on/lan"
+KASA_DEVICE_PORT_NAME="name of port your want to control"
+
+ex:
+PYTHON_PATH="/home/user/anaconda3/bin/python"
+KASA_PATH="/home/user/anaconda3/bin/kasa"
+KASA_DEVICE_IP="123.123.123.123"
+KASA_DEVICE_PORT_NAME="Laptop"
+About_User_File
+
+# read variables from /path/of/script/user_info
+. $(dirname $0)/user_info
+
+# execute every 5 mins until user interrupts
+while true
+do
+  # get current charge level using tlp-stat -b, store the information to a file called state.
+  sudo tlp-stat -b | grep "Charge" > state
+
+  # exetract exact level using python, could probably be done by bash file.
+  battery_level=$($PYTHON_PATH power-check.py)
+
+  # Check if battery level is lower than LOWER_THRES
+  if [ "`echo "${battery_level} < $LOWER_THRES" | bc`" -eq 1 ]
+  then
+    echo "Current: ${battery_level}, Turning ${KASA_DEVICE_PORT_NAME} on."
+    $KASA_PATH --host $KASA_DEVICE_IP on --name $KASA_DEVICE_PORT_NAME
+  
+  # Check if battery level is higher than UPPER_THRES
+  elif [ "`echo "${battery_level} > ${UPPER_THRES}" | bc`" -eq 1 ]
+  then
+    echo "Current: ${battery_level}, Turning ${KASA_DEVICE_PORT_NAME} off."
+    $KASA_PATH --host $KASA_DEVICE_IP off --name $KASA_DEVICE_PORT_NAME
+  
+  # If none of both is true, report the current battery_level
+  else
+    echo "Status keep, ${LOWER_THRES} < ${battery_level} < ${UPPER_THRES}"
+  fi
+  
+  # Wait for 5 mins to continue the next round.
+  sleep 300
+done
+
