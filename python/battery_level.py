a542e80f06ee4c8811921e10cc4b26a92a710250
Qiucha <qiucha86239@gmail.com>
2024-03-28 18:08:44 +0800

    update for directory updates

--- python/battery_level.py
+++ python/battery_level.py
@@ -6,7 +6,7 @@ FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
     # read the state file
-    f = open(f'{FILE_PATH}/generated/charge_level', 'r')
+    f = open(f'{FILE_PATH}/../generated/charge_level', 'r')
 
     # use re to extract the digits
     x = re.search("\d+\.\d*", f.read())
--- python/plug_state.py
+++ python/plug_state.py
@@ -6,7 +6,7 @@ FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
     # read the state file
-    f = open(f'{FILE_PATH}/generated/plug_state', 'r')
+    f = open(f'{FILE_PATH}/../generated/plug_state', 'r')
 
     # use re to extract the digits
     x = re.search("True|False", f.read())
